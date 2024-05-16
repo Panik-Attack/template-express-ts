d33481681e6447bba201bcd3009af0f23f964f71
Eduardo Javier Garcia Alfonso <eduardojga.work@gmail.com>
2024-05-15 22:35:16 -0400

    FEAT: Routes added

---
+++ src/routes/quiz.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router()
+
+
+
+// To get a quiz
+const getQuiz = router.get('/quiz', )
+
+// To send a answer
+const sendAnswer = router.post('/quiz', )
